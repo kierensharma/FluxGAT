b561e84750217bc5cc7afc91f3fa931c162ea320
Kieren Sharma <kieren.sharma@gmail.com>
2024-03-19 14:32:35 +0000

    Changed model directory

--- sampling/flux_sampling.py
+++ sampling/flux_sampling.py
@@ -1,6 +1,6 @@
 import cobra
 from cobra.sampling import sample
 
-model = cobra.io.read_sbml_model("iCHO2291.xml")
+model = cobra.io.read_sbml_model("data/iCHO2291.xml")
 
 s = sample(model, 50000, thinning=1000)
